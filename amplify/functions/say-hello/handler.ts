4c1a429ac87048f29baf2d4edbfbd533c958d738
Salama <elmohamedahmedsalama@gmail.com>
2024-08-06 13:46:14 +0300

    function sayHello with Handler type imported

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -4,3 +4,7 @@ export const handler: Handler = async (event: any) => {
   const { name } = event.arguments
   return `Hello, ${name}!`
 }
+
+// export const handler: Handler  = async (event: any) => {
+//     return "Hello from my first function!";
+//   };
