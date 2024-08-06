e0a9af6ad4c3feaeea9f357f6b7ec34a0d51014c
Salama <elmohamedahmedsalama@gmail.com>
2024-08-06 13:44:34 +0300

    function sayHello with Handler type imported

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -1,10 +1,6 @@
 import type { Handler } from 'aws-lambda';
 
-// export const handler: Handler = async (event: any) => {
-//   const { name } = event.arguments
-//   return `Hello, ${name}!`
-// }
-
-export const handler: Handler  = async (event: any) => {
-    return "Hello from my first function!";
-  };
+export const handler: Handler = async (event: any) => {
+  const { name } = event.arguments
+  return `Hello, ${name}!`
+}
